7f1a7ee26940604b74a90958e574eb635da61f94
jewoos2921 <jewoos2921@gmail.com>
2022-01-22 18:01:07 +0900

    page 147

--- ast/ast.go
+++ ast/ast.go
@@ -237,3 +237,29 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.Body.String())
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token // 여는 괄호 토큰 '('
+	Function  Expression  // 식별자이거나 함수 리터럴
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	var args []string
+	//params:= []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
