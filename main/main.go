22c72bedf40d44cab622c24fa4aa4606b1802d91
jewoos2921 <jewoos2921@gmail.com>
2022-01-22 14:15:21 +0900

    page 29

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"MonkeyKids/repl"
+	"fmt"
+	"os"
+	user2 "os/user"
+)
+
+func main() {
+	user, err := user2.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s ! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stderr, os.Stdout)
+
+}
