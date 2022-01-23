44484bf861d8049ebc1f9754205aa0776104dae7
jewoos2921 <jewoos2921@gmail.com>
2022-01-23 13:59:34 +0900

    page 184

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -8,6 +8,7 @@ import (
 // true나 false를 만날때마다 object.Boolean을 다시 만들어야 하는가??
 // 미리 만들어논 reference를 사용하자
 var (
+	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
 )
@@ -23,6 +24,10 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 
 	// 표현식
 	case *ast.IntegerLiteral:
@@ -45,4 +50,38 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+
+	default:
+		return nil
+	}
+}
+
+// !을 처리하는 동작을 구현
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
