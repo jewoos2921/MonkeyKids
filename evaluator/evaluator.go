151011bfe25cf35aeddcf19088c6d3e0fe91729e
jewoos2921 <jewoos2921@gmail.com>
2022-01-24 13:41:11 +0900

    page 252 문자열 결합 panic: runtime error: invalid memory address or nil pointer dereference [recovered] 	panic: runtime error: invalid memory address or nil pointer dereference 	occur

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -153,6 +153,8 @@ func evalInfixExpression(operator string,
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OJB && right.Type() == object.STRING_OJB:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
@@ -316,4 +318,13 @@ func unwrapReturnValue(obj object.Object) object.Object {
 		return returnValue.Value
 	}
 	return obj
+}
+
+func evalStringInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
