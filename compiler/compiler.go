9832a31f50893beccbd20fc3718803a22dce32d2
jewoos2921 <jewoos2921@gmail.com>
2022-01-27 17:51:13 +0900

    compiler book page 143 chapter 4 조건식

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -7,13 +7,18 @@ import (
 	"fmt"
 )
 
+// 컴파일러를 고쳐서 마지막으로 배출한 명령어 둘을 추적해야한다.
+// 두명령어를 갖는 명령코드와 배출된 위치를 추적할 수 있어야 한다.
 type Compiler struct {
-	instructions code.Instructions
-	constants    []object.Object
+	instructions        code.Instructions
+	constants           []object.Object
+	lastInstruction     EmittedInstruction // 가장 마지막으로 배출한 명려어
+	previousInstruction EmittedInstruction // lastInstruction 직전에 배출된 명령어
 }
 
 func New() *Compiler {
-	return &Compiler{instructions: code.Instructions{}, constants: []object.Object{}}
+	return &Compiler{instructions: code.Instructions{}, constants: []object.Object{},
+		lastInstruction: EmittedInstruction{}, previousInstruction: EmittedInstruction{}}
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
@@ -109,10 +114,24 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 		// OpJumpNotTruthy 명령어에 쓰레기값 9999를 넣어서 배출
 		c.emit(code.OpJumpNotTruthy, 9999)
+		// node.Consequence 은 표현식
+		// node.Consequence 에서 마지막 OpPop 명령어만 제거해야 한다.
 		err = c.Compile(node.Consequence)
 		if err != nil {
 			return err
 		}
+
+		if c.lastInstructionIsPop() {
+			c.removeLastPop()
+		}
+	case *ast.BlockStatement:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
 	}
 
 	return nil
@@ -130,6 +149,10 @@ type Bytecode struct {
 func (c *Compiler) emit(op code.Opcode, operands ...int) int {
 	ins := code.Make(op, operands...)
 	pos := c.addInstruction(ins)
+
+	// 두 필드를 만들어야 한다.
+	c.setLastInstruction(op, pos)
+
 	return pos
 }
 
@@ -142,4 +165,29 @@ func (c *Compiler) addInstruction(ins []byte) int {
 	posNewInstruction := len(c.instructions)
 	c.instructions = append(c.instructions, ins...)
 	return posNewInstruction
+}
+
+type EmittedInstruction struct {
+	Opcode   code.Opcode
+	Position int
+}
+
+// 마지막 명령어가 가진 명령 코드를 타입 안정성(명령코드를 byte로 변환하거나, byte를 명령코드로 변환할 필요가 없다는 뜻)있게 검사할 수 있다.
+func (c *Compiler) setLastInstruction(op code.Opcode, pos int) {
+	previous := c.lastInstruction
+	last := EmittedInstruction{Opcode: op, Position: pos}
+
+	c.previousInstruction = previous
+	c.lastInstruction = last
+}
+
+// 마지막 명령코드가 OpPop 인지 검사한다.
+func (c *Compiler) lastInstructionIsPop() bool {
+	return c.lastInstruction.Opcode == code.OpPop
+}
+
+// 	c.instructions에서 마지막 명령어를 잘라내고 	c.instructions을 previousInstruction로 바꾼다.
+func (c *Compiler) removeLastPop() {
+	c.instructions = c.instructions[:c.lastInstruction.Position]
+	c.lastInstruction = c.previousInstruction
 }
