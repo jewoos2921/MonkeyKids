113db35c1b72cc1223211acf86b472a45cc87529
jewoos2921 <jewoos2921@gmail.com>
2022-01-28 12:58:37 +0900

    compiler book page 194 chapter 5 이름을 추적하는 방법

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"MonkeyKids/compiler"
 	"MonkeyKids/lexer"
+	"MonkeyKids/object"
 	"MonkeyKids/parser"
 	"MonkeyKids/vm"
 	"bufio"
@@ -18,6 +19,10 @@ const PROMPT = ">>"
 // 가장 위에 있는 객체를 출력하도록 바꾸면 된다.
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -36,14 +41,17 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		comp := compiler.New()
+		comp := compiler.NewWithStates(symbolTable, constants)
 		err := comp.Compile(program)
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
 			continue
 		}
 
-		machine := vm.New(comp.Bytecode())
+		code := comp.Bytecode()
+		constants = code.Constants
+		machine := vm.NewWithGlobalsStore(code, globals)
+
 		err = machine.Run()
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
